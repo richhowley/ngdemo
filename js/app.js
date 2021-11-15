e45c294042fef2a4b29889110dc81759acadbb4a
richhowley <howleyrich@gmail.com>
2021-11-15 12:30:35 -0500

    comments

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
     
     var app = angular.module('app', []);
 
+    // call API and return promise
 	app.service('nps', ['$http', function($http) {
 					
 		this.getParkPromise = function(stateCode) {
@@ -15,13 +16,15 @@
 		function($scope,$http, nps) {
 
 			let _this = this;
-			this.parkData = null;
+			this.parkData = null;       // data on all parks in state
 			this.stateCodes = ['ME', 'VT', 'NH', 'MA', 'CT', 'RI'];
-			this.currentState = null;
-			this.currentPark = null;
+			this.currentState = null;   // state selected from pulldown
+			this.currentPark = null;    // park selected from main screen
 			
+            // called when a state is slected in the dropdown
 			this.stateSelected = function()
 			{
+                // get park date for state 
 				nps.getParkPromise(this.currentState).then(function(response) {				   
 					_this.parkData = response.data.data;	
 				
